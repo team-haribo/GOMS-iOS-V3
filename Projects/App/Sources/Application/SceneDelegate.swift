02be4e0e1f95a7b40963724f02df2fd56e7e07f4
minsun <s25055@gsm.hs.kr>
2026-02-21 19:46:41 +0900

    feat: SceneDelegate 시작화면 설정 복구

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -13,13 +13,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-            guard let windowScene = (scene as? UIWindowScene) else { return }
-            window = UIWindow(windowScene: windowScene)
-            
-            // 기존 MainViewController 대신 MapViewController로 시작 화면 설정
-            window?.rootViewController = UINavigationController(rootViewController: MapViewController())
-            window?.makeKeyAndVisible()
-        }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let mainVC = MainViewController()
+        let nav = UINavigationController(rootViewController: mainVC)
+        
+        nav.isNavigationBarHidden = true
+        
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+    }
     
     func sceneDidDisconnect(_ scene: UIScene) {}
 
