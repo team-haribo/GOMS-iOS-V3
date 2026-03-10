b2f47a0f89a95b0b3c3ea8370b0661ce1fd1f101
김준표juunpy0 <junpyokim0106@gmail.com>
2026-03-10 19:15:30 +0900

    ✨-feat :: [#26] Feature 데이터 모델 구조 추가

---
+++ Projects/Feature/Sources/Data/Gender.swift
@@ -0,0 +1,14 @@
+//
+//  Gender.swift
+//  Feature
+//
+//  Created by 김준표 on 2/25/26.
+//  Copyright © 2026 HARIBO. All rights reserved.
+//
+
+import Foundation
+
+enum Gender: String {
+    case man = "MAN"
+    case woman = "WOMAN"
+}
