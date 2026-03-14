048c55418bb5edb49f56c988a5bb13b2c915f6c2
minsun <s25055@gsm.hs.kr>
2026-03-15 05:33:18 +0900

    💄 Add :: MapRouteDetailModel

--- Projects/Feature/Sources/Scene/Map/Model/MapRouteDetailModel.swift
+++ Projects/Feature/Sources/Scene/Map/Model/MapRouteDetailModel.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+public enum RouteTurnType {
+    case start
+    case straight
+    case left
+    case right
+    case end
+    
+    var iconName: String {
+        switch self {
+        case .start: return "ic_route_location_pin"
+        case .straight: return "ic_route_straight"
+        case .left: return "ic_route_turn_left"
+        case .right: return "ic_route_turn_right"
+        case .end: return "ic_route_location_pin"
+        }
+    }
+}
+
+// 빌드 에러 수정을 위한 모델 정의
+struct RouteStepModel {
+    let turnType: RouteTurnType
+    let title: String
+    let description: String
+    
+    var iconName: String {
+        return turnType.iconName
+    }
+}
