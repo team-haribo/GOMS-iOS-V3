814f27f7ddf7ec67d8c32317b42ea15eb13941a8
김준표juunpy0 <junpyokim0106@gmail.com>
2026-03-16 14:17:46 +0900

    ✨-feat :: [#32] AdminMainViewController 메인 화면과 동일한 UI 구조 적용

--- Projects/Feature/Sources/Scene/Main/Admin/View/AdminMainViewController.swift
+++ Projects/Feature/Sources/Scene/Main/Admin/View/AdminMainViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 public class AdminMainViewController: BaseViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
@@ -45,20 +46,13 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
     private let latecomerLabel = UILabel().then {
         $0.text = "지각자 TOP 3"
         $0.setDynamicTextColor(darkModeColor: .white, lightModeColor: .black)
-        $0.font = UIFont.suit(size: 19, weight: .bold)
+        $0.font = UIFont.suit(size: 18, weight: .semibold)
     }
 
-    private lazy var moreLateButton = UIButton().then {
-        $0.backgroundColor = .color.mainText.color.withAlphaComponent(0.1)
-        $0.setTitle("더보기", for: .normal)
-        $0.setTitleColor(.color.gomsSecondary.color, for: .normal)
-        $0.titleLabel?.font = .suit(size: 12, weight: .regular)
-        $0.layer.cornerRadius = 8
-        $0.layer.masksToBounds = true
-        $0.addTarget(self, action: #selector(moreLateButtonTapped), for: .touchUpInside)
-    }
 
     let lateNilView = LateNilView()
+
+    private let outingView = UIView()
 
     lazy var latecomerCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout.init()).then {
         $0.isScrollEnabled = false
@@ -69,23 +63,23 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
 
     private let outingStatusLabel = UILabel().then {
         $0.text = "외출현황"
-        $0.setDynamicTextColor(darkModeColor: .white, lightModeColor: .black)
-        $0.font = UIFont.suit(size: 19, weight: .bold)
+        $0.textColor = .color.mainText.color
+        $0.font = UIFont.suit(size: 18, weight: .semibold)
     }
 
     private lazy var moreOutingStatusButton = UIButton().then {
-        $0.backgroundColor = .color.mainText.color.withAlphaComponent(0.1)
+        $0.backgroundColor = .color.surface.color
         $0.setTitle("더보기", for: .normal)
-        $0.setTitleColor(.color.gomsSecondary.color, for: .normal)
-        $0.titleLabel?.font = .suit(size: 12, weight: .regular)
-        $0.layer.cornerRadius = 8
+        $0.setTitleColor(.color.sub2.color, for: .normal)
+        $0.titleLabel?.font = .suit(size: 13, weight: .regular)
+        $0.layer.cornerRadius = 4
         $0.layer.masksToBounds = true
         $0.addTarget(self, action: #selector(moreOutingStatusButtonTapped), for: .touchUpInside)
     }
 
     let outingCountLabel = UILabel().then {
         $0.textColor = .color.sub2.color
-        $0.font = UIFont.suit(size: 12, weight: .regular)
+        $0.font = UIFont.suit(size: 14, weight: .medium)
     }
 
     lazy var outingStatusCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout.init()).then {
@@ -103,7 +97,7 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
     private func setupNavigationBar() {
         self.navigationController?.navigationBar.prefersLargeTitles = false
         self.navigationItem.hidesBackButton = true
-        self.navigationController?.navigationBar.isHidden = true
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
     private var isVisible: Bool = false
@@ -111,9 +105,17 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
     // MARK: - Life Cycle
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        navigationController?.setNavigationBarHidden(true, animated: false)
+
         isVisible = true
         fetchData()
         setupNavigationBar()
+    }
+
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
     public override func viewWillDisappear(_ animated: Bool) {
@@ -124,33 +126,16 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
 
     public override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.setHidesBackButton(true, animated: false)
+        navigationController?.navigationBar.topItem?.hidesBackButton = true
         self.latecomerCollectionView.reloadData()
         self.outingStatusCollectionView.reloadData()
-        handleRefreshControl()
         configureRefreshControl()
-        setupScrollView()
         refreshControl.beginRefreshing()
+        fetchData()
+        setupNavigationBar()
     }
 
-    func setupScrollView() {
-        view.addSubview(scrollView)
-        scrollView.addSubview(contentView)
-
-        scrollView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-
-        contentView.snp.makeConstraints { make in
-            make.edges.equalTo(scrollView.contentLayoutGuide)
-            make.width.equalTo(scrollView.frameLayoutGuide)
-        }
-
-        contentView.snp.makeConstraints { make in
-            make.height.greaterThanOrEqualTo(view.snp.height).priority(.low)
-        }
-
-        addView()
-    }
 
     func configureRefreshControl() {
         scrollView.refreshControl = refreshControl
@@ -159,15 +144,7 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
     }
 
     @objc func handleRefreshControl() {
-      
-        let adminVC = AdminMainViewController()
-        if !(self.navigationController?.topViewController is AdminMainViewController) {
-            self.navigationController?.setViewControllers([adminVC], animated: false)
-        }
-        self.refreshControl.endRefreshing()
-        return
         fetchData()
-
         guard let isLocalEmail = UserDefaults.standard.string(forKey: "localEmail"),
               let isLocalPass = UserDefaults.standard.string(forKey: "localPass") else {
             print("localEmail 또는 localPass 값이 없습니다.")
@@ -276,10 +253,10 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
 
     // MARK: - Setting
     func setup() {
-        if self.viewModel.lateListDatas.count >= 1 {
+        if self.viewModel.lateListDatas.isEmpty {
             lateNilView.isHidden = true
         } else {
-            lateNilView.isHidden = false
+            lateNilView.isHidden = false 
         }
 
         self.setCollectionView()
@@ -299,11 +276,13 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
     }
 
     func setupCountLable() {
-        let attributedString = NSMutableAttributedString(string: "\(self.viewModel.outingListDatas.count)명이 외출 중")
-        let range = (attributedString.string as NSString).range(of: "\(self.viewModel.outingListDatas.count)")
+        let count = viewModel.outingListDatas.count
+
+        let attributedString = NSMutableAttributedString(string: "\(count)명이 외출 중")
+        let range = (attributedString.string as NSString).range(of: "\(count)")
 
         attributedString.addAttribute(.foregroundColor, value: UIColor.color.admin.color, range: range)
-        attributedString.addAttribute(.font, value: UIFont.suit(size: 12, weight: .semibold), range: range)
+        attributedString.addAttribute(.font, value: UIFont.suit(size: 14, weight: .medium), range: range)
 
         self.outingCountLabel.attributedText = attributedString
     }
@@ -368,92 +347,103 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
         }
     }
 
-    @objc func moreLateButtonTapped() {
-        let lateVC = LatecomerListViewController()
-        navigationController?.pushViewController(lateVC, animated: true)
-    }
 
     // MARK: - Configure UI
     public override func configureUI() {
         qrButton.layer.cornerRadius = qrButton.frame.size.width / 2
-        qrButton.clipsToBounds = true
-    }
+        qrButton.clipsToBounds = false
+
+        qrButton.layer.shadowColor = UIColor.color.admin.color.cgColor
+        qrButton.layer.shadowOpacity = 0.8
+        qrButton.layer.shadowRadius = 13
+        qrButton.layer.shadowOffset = CGSize(width: 0.81, height: 0.81)    }
 
     // MARK: - Add View
-    public  override func addView() {
-        [profileView, basicsProfileView, latecomerLabel, moreLateButton, lateNilView, latecomerCollectionView, outingStatusLabel, moreOutingStatusButton, outingCountLabel, outingStatusCollectionView, qrButton].forEach { self.contentView.addSubview($0) }
-        [logo, adminMenuButton, content].forEach { self.contentView.addSubview($0) }
-        contentView.bringSubviewToFront(qrButton)
-        contentView.bringSubviewToFront(moreOutingStatusButton)
-        contentView.bringSubviewToFront(moreLateButton)
+    public override func addView() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+
+        [outingStatusLabel, moreOutingStatusButton, outingCountLabel, outingStatusCollectionView].forEach { self.outingView.addSubview($0) }
+        [logo, adminMenuButton, profileView, basicsProfileView, latecomerLabel, lateNilView, latecomerCollectionView, outingView].forEach { self.contentView.addSubview($0) }
+        view.addSubview(qrButton)
     }
 
     // MARK: - Layout
-    public  override func setLayout() {
+    public override func setLayout() {
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+
+        contentView.snp.makeConstraints { make in
+            make.edges.equalTo(scrollView.contentLayoutGuide)
+            make.width.equalTo(scrollView.frameLayoutGuide)
+            make.height.greaterThanOrEqualTo(view.snp.height).priority(.low)
+        }
+
+        qrButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(120)
+            $0.height.width.equalTo(64)
+        }
+
         logo.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(20)
-            $0.top.equalToSuperview().inset(20)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
             $0.height.equalTo(24)
             $0.width.equalTo(87)
         }
 
         adminMenuButton.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(20)
+            $0.centerY.equalTo(logo.snp.centerY)
             $0.trailing.equalToSuperview().inset(20)
-            $0.height.equalTo(18)
-            $0.width.equalTo(20)
-        }
-
-        content.snp.makeConstraints {
-            $0.top.equalTo(logo.snp.bottom).offset(40)
-            $0.leading.equalTo(bounds.width * 0.05)
-            $0.trailing.equalTo(-(bounds.width * 0.05))
-            $0.bottom.equalToSuperview()
+            $0.width.height.equalTo(24)
         }
 
         updateLayout()
 
         lateNilView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(20)
-            $0.height.equalTo(40)
-            $0.top.equalTo(latecomerLabel.snp.bottom).offset(8)
+            $0.height.equalTo(20)
+            $0.top.equalTo(latecomerLabel.snp.bottom)
         }
 
         latecomerCollectionView.snp.makeConstraints {
-            $0.top.equalTo(latecomerLabel.snp.bottom).offset(8)
+            $0.top.equalTo(lateNilView.snp.bottom).offset(12)
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(136)
+        }
+
+        outingView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(latecomerCollectionView.snp.bottom).offset(24)
+            $0.bottom.equalTo(contentView.snp.bottom).offset(-100)
         }
 
         outingStatusLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(20)
             $0.height.equalTo(32)
-            $0.top.equalTo(latecomerCollectionView.snp.bottom).offset(24)
+            $0.top.equalToSuperview()
         }
 
         outingCountLabel.snp.makeConstraints {
-            $0.top.equalTo(latecomerCollectionView.snp.bottom).offset(24)
+            $0.top.equalToSuperview()
             $0.leading.equalTo(outingStatusLabel.snp.trailing).offset(8)
             $0.height.equalTo(32)
         }
 
         moreOutingStatusButton.snp.makeConstraints {
-            $0.top.equalTo(latecomerCollectionView.snp.bottom).offset(28)
+            $0.top.equalToSuperview().offset(4)
             $0.trailing.equalToSuperview().inset(20)
             $0.width.equalTo(48)
             $0.height.equalTo(24)
         }
 
         outingStatusCollectionView.snp.makeConstraints {
-            $0.top.equalTo(outingStatusLabel.snp.bottom).offset(16)
-            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(outingStatusLabel.snp.bottom).offset(17)
+            $0.leading.trailing.equalToSuperview().inset(24)
             $0.bottom.equalToSuperview()
-        }
-
-        qrButton.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(20)
-            $0.bottom.equalToSuperview().inset(130)
-            $0.height.width.equalTo(64)
         }
     }
 
@@ -468,13 +458,6 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
             $0.top.equalTo(profileView.snp.bottom).offset(24)
             $0.leading.equalToSuperview().inset(20)
             $0.height.equalTo(32)
-        }
-
-        moreLateButton.snp.makeConstraints {
-            $0.top.equalTo(profileView.snp.bottom).offset(24)
-            $0.trailing.equalToSuperview().inset(20)
-            $0.width.equalTo(48)
-            $0.height.equalTo(24)
         }
 
         basicsProfileView.snp.remakeConstraints {
@@ -496,7 +479,7 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
 
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == latecomerCollectionView {
-            return viewModel.lateListDatas.count
+            return viewModel.lateListDatas.isEmpty ? 3 : viewModel.lateListDatas.count
         } else if collectionView == outingStatusCollectionView {
             return viewModel.outingListDatas.count
         }
@@ -506,8 +489,14 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == latecomerCollectionView {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LateCell.identifier, for: indexPath) as? LateCell else { return UICollectionViewCell() }
-            let data = viewModel.lateListDatas[indexPath.row]
-            cell.configure(with: data)
+
+            if viewModel.lateListDatas.isEmpty {
+                cell.configureDummy()
+            } else {
+                let data = viewModel.lateListDatas[indexPath.row]
+                cell.configure(with: data)
+            }
+
             return cell
         } else if collectionView == outingStatusCollectionView {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OutingStatusCollectionViewCell.identifier, for: indexPath) as? OutingStatusCollectionViewCell else { return UICollectionViewCell() }
@@ -523,12 +512,12 @@ public class AdminMainViewController: BaseViewController, UICollectionViewDataSo
 extension AdminMainViewController {
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == latecomerCollectionView {
-            let width = bounds.width * 0.27
+            let width = view.bounds.width * 0.27
             let height: CGFloat = 136
             return CGSize(width: width, height: height)
         } else if collectionView == outingStatusCollectionView {
-            let width = bounds.width * 0.9
-            let height: CGFloat = 50
+            let width = collectionView.bounds.width
+            let height: CGFloat = 44
             return CGSize(width: width, height: height)
         }
         return CGSize(width: 0, height: 0)
@@ -536,10 +525,32 @@ extension AdminMainViewController {
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         if collectionView == latecomerCollectionView {
-            return bounds.width * 0.03
+            return view.bounds.width * 0.03
         } else if collectionView == outingStatusCollectionView {
             return 0
         }
         return 0
+    }
+
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        if collectionView == outingStatusCollectionView {
+            return 4
+        }
+        return 10
+    }
+
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+        if collectionView == outingStatusCollectionView {
+            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 4)
+        }
+        return .zero
     }
 }
