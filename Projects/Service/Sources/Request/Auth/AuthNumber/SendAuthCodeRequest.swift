3d5e04b8336bff90e85974dbdf852757d847576b
김준표juunpy0 <junpyokim0106@gmail.com>
2026-03-27 10:39:04 +0900

    ✨-feat :: [#92] Auth API Service 구현 및 DTO 구성 (signup, signin, email 인증)

--- Projects/Service/Sources/Request/Auth/AuthNumber/SendAuthCodeRequest.swift
+++ Projects/Service/Sources/Request/Auth/AuthNumber/SendAuthCodeRequest.swift
@@ -1,8 +1,8 @@
 //
-//  SendAuthNumberRequest.swift
+//  SendAuthCodeRequest.swift
 //  Service
 //
-//  Created by 김준표 on 2/24/26.
+//  Created by 김준표 on 3/27/26.
 //  Copyright © 2026 HARIBO. All rights reserved.
 //
 
@@ -10,10 +10,10 @@ import Foundation
 
 public struct SendAuthCodeRequest: Codable {
     var email: String
-    var emailStatus: String
+    var purpose: String
     
-    public init(email: String, emailStatus: String) {
+    public init(email: String, purpose: String) {
         self.email = email
-        self.emailStatus = emailStatus
+        self.purpose = purpose
     }
 }
--- Projects/Service/Sources/Request/Auth/AuthNumber/VerifyAuthNumberRequest.swift
+++
@@ -1,8 +0,0 @@
-//
-//  VerifyAuthNumberRequest.swift
-//  Service
-//
-//  Created by 김준표 on 2/24/26.
-//  Copyright © 2026 HARIBO. All rights reserved.
-//
-
