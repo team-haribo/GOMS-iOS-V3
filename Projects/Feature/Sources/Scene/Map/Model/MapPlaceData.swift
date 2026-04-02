90b512d34f00d81e01d400b3e0f9005c9d48f12b
minsun <s25055@gsm.hs.kr>
2026-04-02 21:10:45 +0900

    ♻️ - refactor :: API 명세서 기반 MapPlaceData 모델 변수명 및 구조 수정

--- Projects/Feature/Sources/Scene/Map/Model/MapPlaceData.swift
+++ Projects/Feature/Sources/Scene/Map/Model/MapPlaceData.swift
@@ -8,23 +8,36 @@
 
 import Foundation
 
-public struct MapReview {
-    public let name: String
-    public let info: String
-    public let content: String
-    public let date: String
-    public let isMine: Bool
+// MARK: - Search Response
+public struct MapPlaceResponse: Codable {
+    public let places: [MapPlaceData]
     
-    public init(name: String, info: String, content: String, date: String, isMine: Bool = false) {
-        self.name = name
-        self.info = info
-        self.content = content
-        self.date = date
-        self.isMine = isMine
+    public init(places: [MapPlaceData]) {
+        self.places = places
     }
 }
 
-public struct MapPlaceDetailData {
+// MARK: - Place Data (검색 목록용)
+public struct MapPlaceData: Codable {
+    public let latitude: Double
+    public let longitude: Double
+    public let placeId: Int
+    public let reviewCount: Int
+    public let recommendCount: Int
+    public let recommended: Bool
+
+    public init(latitude: Double, longitude: Double, placeId: Int, reviewCount: Int, recommendCount: Int, recommended: Bool) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.placeId = placeId
+        self.reviewCount = reviewCount
+        self.recommendCount = recommendCount
+        self.recommended = recommended
+    }
+}
+
+// MARK: - Place Detail Data (상세 페이지용)
+public struct MapPlaceDetailData: Codable {
     public let title: String
     public let category: String
     public let address: String
@@ -43,5 +56,22 @@ public struct MapPlaceDetailData {
         self.reviewCount = reviewCount
         self.recommendationCount = recommendationCount
         self.reviews = reviews
+    }
+}
+
+// MARK: - Review Data
+public struct MapReview: Codable {
+    public let name: String
+    public let info: String
+    public let content: String
+    public let date: String
+    public let isMine: Bool
+    
+    public init(name: String, info: String, content: String, date: String, isMine: Bool = false) {
+        self.name = name
+        self.info = info
+        self.content = content
+        self.date = date
+        self.isMine = isMine
     }
 }
