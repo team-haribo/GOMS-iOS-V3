dafe8bdcd97977959371a736132938bbaa1348d4
minsun <s25055@gsm.hs.kr>
2026-04-04 09:33:15 +0900

    feat: 카카오맵 SDK 설정 추가

--- Projects/ThirdPartyLib/Project.swift
+++ Projects/ThirdPartyLib/Project.swift
@@ -29,7 +29,7 @@ let project = Project.makeModule(
         .remote(url: "https://github.com/yannickl/QRCodeReader.swift.git",
                 requirement: .upToNextMajor(from: "10.1.0")),
         
-        // 카카오맵 패키지 주소 추가
+        // 카카오맵 패키지
         .remote(url: "https://github.com/kakao-maps-sdk/KakaoMapsSDK-SPM.git",
                 requirement: .upToNextMajor(from: "2.10.0")),
         
@@ -46,7 +46,7 @@ let project = Project.makeModule(
         .package(product: "QRCode"),
         .package(product: "QRCodeReader"),
         
-        // 카카오맵 의존성 추가
+        // 카카오맵 의존성
         .package(product: "KakaoMapsSDK"),
       
         .package(product: "FirebaseCore"),
