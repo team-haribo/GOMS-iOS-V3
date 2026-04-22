d7de77f9890ab3ad43ced7e3a85cd120bd6e9643
김준표juunpy0 <junpyokim0106@gmail.com>
2026-04-23 01:21:34 +0900

    💄-fix :: [#182] 바텀시트 후기 삭제 시 알럿 이전에 UI가 먼저 반영되던 문제 수정

--- Projects/Feature/Sources/Scene/Map/View/MapBottomSheetView.swift
+++ Projects/Feature/Sources/Scene/Map/View/MapBottomSheetView.swift
@@ -212,8 +212,7 @@ public final class MapBottomSheetView: UIView {
         guard index < reviewPlaces.count else { return }
         let placeId = reviewPlaces[index].id
         onDeleteTapped?(placeId)
-        reviewPlaces.remove(at: index)
-        renderUI()
+        // 삭제는 ViewController에서 API 성공 후 데이터 갱신으로 처리
     }
 
     @objc private func didTapCard(_ sender: UITapGestureRecognizer) {
