d228b959a15a05ed39ab79fd922e9405bf989eef
김민선 <minsun@gimminseon-ui-MacBookPro.local>
2026-06-17 19:55:07 +0900

    🐛 - fix :: 지각자 명단 조회 시 당일 날짜 미반영 및 계산 오류 수정

--- Projects/Feature/Sources/Scene/Late/ViewModel/LetecomerViewModel.swift
+++ Projects/Feature/Sources/Scene/Late/ViewModel/LetecomerViewModel.swift
@@ -23,13 +23,10 @@ public final class LetecomerViewModel: BaseViewModel {
     private let studentCouncilProvider = MoyaProvider<StudentCouncilServices>()
     
     var date: String = {
-        let currentDate = Date()
-        let lastWednesday = currentDate.lastWednesday()
-        
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         
-        return formatter.string(from: lastWednesday)
+        return formatter.string(from: Date())
     }()
     
     var latecomerList: [LatecomerListResponse] = []
